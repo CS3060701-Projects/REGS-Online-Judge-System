048f31868225aa505711c3641c699d0f0c5e9e39
B11415060 <B11415060@mail.ntust.edu.tw>
2026-04-25 19:24:49 +0800

    updated .gitignore

---
+++ cmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"regs-backend/internal/api/handlers" // 引入你的 handlers
+	"regs-backend/internal/database"
+	jwtPkg "regs-backend/pkg/jwt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	database.Connect()
+
+	if err := jwtPkg.InitKeys(); err != nil {
+		log.Fatal("JWT 初始化失敗:", err)
+	}
+
+	r := gin.Default()
+
+	r.GET("/api/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	r.POST("/api/users/register", handlers.Register)
+	r.POST("/api/users/login", handlers.Login)
+	r.POST("/api/submissions", handlers.SubmitCode)
+	r.GET("/api/submissions/:operatorId/status", handlers.GetSubmissionStatus)
+	r.Run(":8081")
+}
